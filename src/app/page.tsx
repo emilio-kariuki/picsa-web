f6559d086151b6a888da28c9875461a293ce5a3d
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-01-30 22:28:48 +0300

    add: added react webcam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
-export default function Home() {
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRef } from "react";
+import Webcam from "react-webcam";
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-        Click me
-        
-      </Button>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#121212] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <Webcam />;
+        <Button>take a picture</Button>
+      </div>
     </main>
-  )
+  );
 }
