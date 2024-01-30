abc4f2ad0a98012e9cca0dbcc88f6883480b21cf
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-01-30 22:46:11 +0300

    add: added react webcam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { useRef } from "react";
+import { Button } from "@/components/ui/button";
 import Webcam from "react-webcam";
 export default function HomePage() {
+  const camera = useRef<Webcam>(null);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#121212] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-      <Webcam />;
-        <Button>take a picture</Button>
+        <Webcam
+          audio={false}
+          height={720}
+          ref={camera}
+          screenshotFormat="image/jpeg"
+          width={1280}
+          videoConstraints={{
+            width: 1280,
+            height: 720,
+            facingMode: "environment",
+          }}
+        />
+        ;<Button>take a picture</Button>
       </div>
     </main>
   );
