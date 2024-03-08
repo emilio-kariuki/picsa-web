4d0eb52431aa1627df6103e7a3ef899377a0d95f
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-03-08 13:28:45 +0300

    add: adds new files"

--- src/app/api/v1/insights/route.ts
+++ src/app/api/v1/insights/route.ts
@@ -2,9 +2,11 @@ import { NextResponse, NextRequest } from "next/server";
 import { supabase } from "@/lib/supabase";
 
 export const GET = async (req: NextRequest, res: NextResponse) => {
+  const phone = req.nextUrl.searchParams.get("phone");
   const { data: event, error } = await supabase
     .from("Insights")
     .select("*")
+    .eq("phone", phone)
     .order("createdAt", { ascending: false });
 
   if (error) {
