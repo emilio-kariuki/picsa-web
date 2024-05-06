3f6afb14a6c4fcb4cb79903aa0fbebfe05b06d0a
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-05-07 00:30:33 +0300

    add: adds the payment webhook

--- src/app/api/v1/payment/route.ts
+++ src/app/api/v1/payment/route.ts
@@ -4,18 +4,16 @@ import { NextResponse, NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
     const data = await req.json();
-    if(data.type == "INITIAL_PURCHASE"){
-        const user = await supabase.from('payment').insert(
-            {
-                "user_id": data.app_user_id,
-                'amount': data.price,
-                'store': data.store,
-                'environment': data.environment,
-                
-            }
-        )
-        console.log(user)
-    }
+    const user = await supabase.from('payment').insert(
+        {
+            "user_id": data.app_user_id,
+            'amount': data.price,
+            'store': data.store,
+            'environment': data.environment,
+            'type': data.type,
+            
+        }
+    )
     return new NextResponse(JSON.stringify(data), {
         headers: {
             "Content-Type": "application/json",
