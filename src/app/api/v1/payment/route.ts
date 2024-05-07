cfa2085ab0e64ee799ddfa1979ddd71a2dc4c809
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-05-07 13:10:04 +0300

    add: adds the payment webhook

--- src/app/api/v1/payment/route.ts
+++ src/app/api/v1/payment/route.ts
@@ -6,7 +6,7 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
     const data = await req.json();
     console.log(data)
     let isPro = data.event.type == "INITIAL_PURCHASE" ? true : data.event.type == "RENEWAL" ? true : data.event.type == "SUBSCRIPTION_EXTENDED" ? true : data.event.type == "TEMPORARY_ENTITLEMENT_GRANT" ? true : false;
-    const user = await supabase.from('user').update({
+    const user = await supabase.from('User').update({
         'pro': isPro
     }).eq('id', data.event.app_user_id)
     console.log(user)
