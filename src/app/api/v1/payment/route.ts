7886233500227e1f69ba746ae964c9daad303469
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-05-07 14:31:40 +0300

    add: adds the payment webhook

--- src/app/api/v1/payment/route.ts
+++ src/app/api/v1/payment/route.ts
@@ -1,19 +1,48 @@
 import { supabase } from "@/lib/supabase";
-import { NextResponse, NextRequest } from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
-    const data = await req.json();
-    console.log(data)
-    let isPro = data.event.type == "INITIAL_PURCHASE" ? true : data.event.type == "RENEWAL" ? true : data.event.type == "SUBSCRIPTION_EXTENDED" ? true : data.event.type == "TEMPORARY_ENTITLEMENT_GRANT" ? true : false;
-    const user = await supabase.from('User').update({
-        'pro': isPro,
-        'token': data.event.subscriber_attributes.$fcmTokens.value
-    }).eq('id', data.event.app_user_id)
-    console.log(user)
-    return new NextResponse(JSON.stringify(data), {
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-}
+  const data = await req.json();
+  console.log(data);
+  let isPro = data.event.type == "INITIAL_PURCHASE"
+    ? true
+    : data.event.type == "RENEWAL"
+    ? true
+    : data.event.type == "SUBSCRIPTION_EXTENDED"
+    ? true
+    : data.event.type == "TEMPORARY_ENTITLEMENT_GRANT"
+    ? true
+    : data.event.type == "CANCELLATION"
+    ? false
+    : data.event.type == "UNCANCELLATION"
+    ? true
+    : data.event.type == "SUBSCRIPTION_PAUSED"
+    ? false
+    : data.event.type == "EXPIRATION"
+    ? false
+    : data.event.type == "BILLING_ISSUE"
+    ? false
+    : data.event.type == "SUBSCRIPTION_EXTENDED"
+    ? true
+    : false;
+  const user = await supabase.from("User").update({
+    "pro": isPro,
+    "token": data.event.subscriber_attributes.$fcmTokens.value,
+    "payment": {
+        "pro" : isPro,
+        "title": isPro ? "Premium Plan" :"Free Plan",
+        "price":  `${data.event.price}`,
+        "expiration": isPro? `${data.event.expiration_at_ms}`: "never",
+        "currency": data.event.currency,
+        "transactionId": isPro? data.event.transaction_id: "none",
+    }
+  }).eq("id", data.event.app_user_id);
+
+  console.log("user payment profile has been updated successfully");
+  return new NextResponse(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
